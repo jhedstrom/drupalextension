a8f03ca18ac814b4101962508d29404f998b191a
Melissa Anderson <melissa@happypunch.com>
2012-05-14 09:40:06 -0700

    Issue #1570010 by eliza411: Implemented first marketplace scenario, reorganzied files

--- DrupalOrgContext.php
+++ DrupalOrgContext.php
@@ -22,7 +22,6 @@ class DrupalOrgContext extends BehatContext
    *   Context parameters (set them up through behat.yml).
    */
   public function __construct(array $parameters) {
-    $this->search_value = 'Masquerade';
     $this->base_url = $parameters['base_url'];
     $driver = new \Behat\Mink\Driver\Selenium2Driver('firefox', array());
     $firefox = new \Behat\Mink\Session($driver);
@@ -40,12 +39,12 @@ class DrupalOrgContext extends BehatContext
   }
   
   /**
-   * @Given /^I am at \'([^\']*)\'$/
+   * @Given /^I am at "([^"]*)"$/
    */
-  public function iAmAt($arg1)
+  public function iAmAt($path)
   {
     $session = $this->mink->getSession();
-    $session->visit($this->base_url .'/project/modules');
+    $session->visit($this->base_url . $path);
 	$status = $session->getStatusCode();
 	if ($status != 200) {
         throw new Exception("Status $status when retriving ". $session->getCurrentUrl());
@@ -56,11 +55,12 @@ class DrupalOrgContext extends BehatContext
   /**
    * @When /^I search for "([^"]*)"$/
    */
-  public function iSearchFor($arg1)
+  public function iSearchFor($searchterm)
   {
+    $this->searchterm = $searchterm;
 	$session = $this->mink->getSession();
     $element = $session->getPage();
-	$element->fillField('edit-text', $this->search_value);
+	$element->fillField('edit-text', $this->searchterm);
 	$submit = $element->findById('edit-submit');
     if (empty($submit)) {
         throw new Exception('No submit button at '. $session->getCurrentUrl());
@@ -75,9 +75,37 @@ class DrupalOrgContext extends BehatContext
   {
 	$session = $this->mink->getSession();
 	$element = $session->getPage();
-	$result = $element->findLink($this->search_value);
+	$result = $element->findLink($this->searchterm);
 	if (empty($result)) {
-		throw new Exception("No link to $this->search_value on ". $session->getCurrentUrl());
+		throw new Exception("No link to $this->searchterm on ". $session->getCurrentUrl());
 	}
   }
+
+  /**
+   * @When /^I click "([^"]*)"$/
+   */
+  public function iClick($linkname)
+  {
+    $session = $this->mink->getSession();
+    $element = $session->getPage();
+    $result = $element->findLink($linkname);
+    if (!empty($result)) {
+      $result->click();
+     }
+   }
+
+  /**
+   * @Then /^I should see a link called "([^"]*)"$/
+   */
+  public function iShouldSeeALinkCalled($linkname)
+  {
+  $session = $this->mink->getSession();
+  $element = $session->getPage();
+  $result = $element->findLink($linkname);
+  if (empty($result)) {
+      throw new Exception("No link to ". $linkname ."  on ". $session->getCurrentUrl());
+    }
+  }
 }
+
+
