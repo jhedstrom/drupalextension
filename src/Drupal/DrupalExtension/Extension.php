de343fe2f85ef2696e1569175e4c7a53fef9eadc
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-07-26 11:34:10 -0700

    Adding initial extension class.

---
+++ src/Drupal/DrupalExtension/Extension.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\DrupalExtension;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\DependencyInjection\Loader\YamlFileLoader,
+    Symfony\Component\Config\FileLocator,
+    Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+use Behat\Behat\Extension\ExtensionInterface;
+
+class Extension implements ExtensionInterface {
+
+  /**
+   * Loads a specific configuration.
+   *
+   * @param array $config
+   *   Extension configuration (from behat.yml).
+   * @param ContainerBuilder $container
+   *   ContainerBuilder instance.
+   */
+  public function load(array $config, ContainerBuilder $container) {
+    // @todo
+    xdebug_break();
+    return;
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+    $loader->load('services.yml');
+
+    $container->setParameter('drupal.region_map', $config['regions']);
+  }
+
+  /**
+   * Setup configuration for this extension.
+   *
+   * @param ArrayNodeDefinition $builder
+   *   ArrayNodeDefinition instance.
+   */
+  public function getConfig(ArrayNodeDefinition $builder) {
+    // @todo
+    return;
+    // defining what type of configuration can be passed into this extension
+    $builder->
+      children()->
+        // this says, have a "regions" key, which takes a key-value array
+        arrayNode('regions')->
+          useAttributeAsKey('key')->
+          prototype('variable')->end()->
+        end()->
+      end()
+    ;
+  }
+
+  /**
+   * Returns compiler passes used by mink extension.
+   *
+   * @return array
+   */
+  public function getCompilerPasses() {
+    // @todo
+    return array();
+    return array(
+      new Compiler\SelectorsPass(),
+      new Compiler\SessionsPass(),
+    );
+  }
+}
