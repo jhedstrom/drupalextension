fb6792b24ebafcd2d3218f05ff356f41f20bfe58
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-08-23 11:50:18 +0200

    Progress on context initializer.

--- src/Drupal/DrupalExtension/Context/Initializer/DrupalAwareInitializer.php
+++ src/Drupal/DrupalExtension/Context/Initializer/DrupalAwareInitializer.php
@@ -1,9 +1,15 @@
 <?php
 
-namespace Drupal\DrupalExtenstion\Context\Initializer;
+namespace Drupal\DrupalExtension\Context\Initializer;
 
 use Behat\Behat\Context\Initializer\InitializerInterface;
+use Behat\Behat\Context\ContextInterface;
 
 class DrupalAwareInitializer implements InitializerInterface {
-  
+  public function initialize(ContextInterface $context) {
+  }
+
+  public function supports(ContextInterface $context) {
+    return $context instanceof DrupalAwareInterface;
+  }
 }
