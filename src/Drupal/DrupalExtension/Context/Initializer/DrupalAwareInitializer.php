bd3e9c7df810aa607918073ddbe6df24dc18951f
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-08-23 11:31:32 +0200

    Initializer placeholder (not working) class.

---
+++ src/Drupal/DrupalExtension/Context/Initializer/DrupalAwareInitializer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\DrupalExtenstion\Context\Initializer;
+
+use Behat\Behat\Context\Initializer\InitializerInterface;
+
+class DrupalAwareInitializer implements InitializerInterface {
+  
+}
