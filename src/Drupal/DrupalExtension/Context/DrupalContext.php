90b9ea33b2b5208a447ecdbecf022f4c07407200
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-08-24 14:21:02 +0200

    Removing unused Behat\Behat\Context\TranslatedContextInterface namespace.

--- src/Drupal/DrupalExtension/Context/DrupalContext.php
+++ src/Drupal/DrupalExtension/Context/DrupalContext.php
@@ -3,8 +3,7 @@
 namespace Drupal\DrupalExtension\Context;
 
 use Behat\MinkExtension\Context\MinkContext;
-use Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
+use Behat\Behat\Context\BehatContext,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
