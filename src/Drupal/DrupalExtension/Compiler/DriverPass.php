9bccf628823c5905b83c43983302cd7834818f38
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-09-13 17:41:46 -0700

    A bit more work on driver pass.

--- src/Drupal/DrupalExtension/Compiler/DriverPass.php
+++ src/Drupal/DrupalExtension/Compiler/DriverPass.php
@@ -2,7 +2,8 @@
 
 namespace Drupal\DrupalExtension\Compiler;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder,
+use Symfony\Component\DependencyInjection\Reference,
+    Symfony\Component\DependencyInjection\ContainerBuilder,
     Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
 /**
@@ -13,8 +14,19 @@ class DriverPass implements CompilerPassInterface {
    * Register Drupal drivers.
    */
   public function process(ContainerBuilder $container) {
+    if (!$container->hasDefinition('drupal.context')) {
+      return;
+    }
+
+    $drupalContextDefinition = $container->getDefinition('drupal.context');
     foreach ($container->findTaggedServiceIds('drupal.context.driver') as $id => $attributes) {
-      // @todo Do something.
+      foreach ($attributes as $attribute) {
+        if (isset($attribute['alias']) && $name = $attribute['alias']) {
+          $drupalContextDefinition->addMethodCall(
+            'registerDriver', array($name, new Reference($id))
+          );
+        }
+      }
     }
   }
 }
