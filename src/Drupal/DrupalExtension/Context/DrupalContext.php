dc4ae9a9958ed997ef23a96e78c9e32d1b6145fc
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-11-13 17:35:08 -0800

    Coding standards fix.

--- src/Drupal/DrupalExtension/Context/DrupalContext.php
+++ src/Drupal/DrupalExtension/Context/DrupalContext.php
@@ -599,11 +599,11 @@ class DrupalContext extends MinkContext implements DrupalAwareInterface {
   }
 
   /**
-    * @Given /^the cache has been cleared$/
-    */
-    public function theCacheHasBeenCleared() {
-      $this->getDriver()->clearCache();
-    }
+   * @Given /^the cache has been cleared$/
+   */
+  public function theCacheHasBeenCleared() {
+    $this->getDriver()->clearCache();
+  }
 
   /**
    * @} End of defgroup "drupal extensions"
