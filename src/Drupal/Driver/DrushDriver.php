9961fb452afeb3958a068c2d91e11798f363ff17
grendzy <grendzy@96647.no-reply.drupal.org>
2013-01-03 17:00:15 -0800

    Issue #1868980 by grendzy: Fixed DrushDriver: ANSI color output confuses XML parsers.

--- src/Drupal/Driver/DrushDriver.php
+++ src/Drupal/Driver/DrushDriver.php
@@ -134,6 +134,7 @@ class DrushDriver implements DriverInterface {
   public function drush($command, array $arguments = array(), array $options = array()) {
     $arguments = implode(' ', $arguments);
     $string_options = '';
+    $options['nocolor'] => '';
     foreach ($options as $name => $value) {
       if (is_null($value)) {
         $string_options .= ' --' . $name;
