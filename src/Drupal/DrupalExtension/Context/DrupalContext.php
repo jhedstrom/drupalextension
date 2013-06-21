440ecd93bdb8c3b4f80f3776d9a26e2bcbbc08be
Michael Nielson <michael@opensourcery.com>
2013-06-20 17:16:54 -0700

    Added breakpoint step definition.  USAGE: Then (I) break

--- src/Drupal/DrupalExtension/Context/DrupalContext.php
+++ src/Drupal/DrupalExtension/Context/DrupalContext.php
@@ -984,4 +984,25 @@ class DrupalContext extends MinkContext implements DrupalAwareInterface {
   /**
    * @} End of defgroup "drupal extensions"
    */
+  /**
+   * @defgroup "debugging steps"
+   * @{
+   */
+
+  /**
+   * Pauses the scenario until the user presses a key. Useful when debugging a scenario.
+   *
+   * @Then /^(?:|I )break$/
+   */
+    public function iPutABreakpoint()
+    {
+      fwrite(STDOUT, "\033[s \033[93m[Breakpoint] Press \033[1;93m[RETURN]\033[0;93m to continue...\033[0m");
+      while (fgets(STDIN, 1024) == '') {}
+      fwrite(STDOUT, "\033[u");
+      return;
+    }
+
+  /**
+   * @} End of defgroup "debugging steps"
+   */
 }
