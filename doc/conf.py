37484a846929945602a6eafee734efdae36b7b04
Melissa Anderson <melissa@happypunch.com>
2014-08-07 17:37:08 -0700

    Themed with Read the Docs theme

--- doc/conf.py
+++ doc/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -92,6 +93,7 @@ exclude_patterns = ['_build']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
 #html_sidebars = {
 #  '**':['globaltoc.html','searchbox.html'],
 #  'using/windows': ['windowssidebar.html', 'searchbox.html'],
@@ -103,6 +105,7 @@ exclude_patterns = ['_build']
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
