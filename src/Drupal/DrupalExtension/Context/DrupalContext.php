bb437f148df226807f494d212551b39ae45c6cc2
danepowell <danepowell@339326.no-reply.drupal.org>
2014-10-21 10:14:07 -0700

    Issue #2328681 by Dane Powell: Added Log in as user with multiple roles.

--- src/Drupal/DrupalExtension/Context/DrupalContext.php
+++ src/Drupal/DrupalExtension/Context/DrupalContext.php
@@ -824,13 +824,13 @@ class DrupalContext extends MinkContext implements DrupalAwareInterface, Transla
   }
 
   /**
-   * Creates and authenticates a user with the given role via Drush.
+   * Creates and authenticates a user with the given role(s) via Drush.
    *
-   * @Given /^I am logged in as a user with the "(?P<role>[^"]*)" role$/
+   * @Given /^I am logged in as a user with the "(?P<roles>[^"]*)" role(?:|s)$/
    */
-  public function assertAuthenticatedByRole($role) {
+  public function assertAuthenticatedByRole($roles) {
     // Check if a user with this role is already logged in.
-    if ($this->loggedIn() && $this->user && isset($this->user->role) && $this->user->role == $role) {
+    if ($this->loggedIn() && $this->user && isset($this->user->role) && $this->user->role == $roles) {
       return TRUE;
     }
 
@@ -838,7 +838,7 @@ class DrupalContext extends MinkContext implements DrupalAwareInterface, Transla
     $user = (object) array(
       'name' => $this->getDrupal()->random->name(8),
       'pass' => $this->getDrupal()->random->name(16),
-      'role' => $role,
+      'role' => $roles,
     );
     $user->mail = "{$user->name}@example.com";
 
@@ -847,11 +847,16 @@ class DrupalContext extends MinkContext implements DrupalAwareInterface, Transla
 
     $this->users[$user->name] = $this->user = $user;
 
-    if ($role == 'authenticated user') {
-      // Nothing to do.
-    }
-    else {
-      $this->getDriver()->userAddRole($user, $role);
+    $roles = explode(',', $roles);
+    $roles = array_map('trim', $roles);
+
+    foreach ($roles as $role) {
+      if ($role == 'authenticated user') {
+        // Nothing to do.
+      }
+      else {
+        $this->getDriver()->userAddRole($user, $role);
+      }
     }
 
     // Login.
