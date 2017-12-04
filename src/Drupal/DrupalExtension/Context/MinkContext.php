fe62a15119ca6710c9e289975c4eeaeecd93041a
Jonathan Hedstrom <jhedstrom@gmail.com>
2017-12-04 14:20:53 -0800

    Merge pull request #429 from jhedstrom/das-peter-iWaitForAjaxToFinish-d7-fix
    Reroll: ajax fix for d7
    
    Signed-off-by: Jonathan Hedstrom <jhedstrom@gmail.com>

--- src/Drupal/DrupalExtension/Context/MinkContext.php
+++ src/Drupal/DrupalExtension/Context/MinkContext.php
@@ -126,11 +126,16 @@ class MinkContext extends MinkExtension implements TranslatableContext {
       function isAjaxing(instance) {
         return instance && instance.ajaxing === true;
       }
+      var d7_not_ajaxing = true;
+      if (typeof Drupal !== 'undefined' && typeof Drupal.ajax !== 'undefined' && typeof Drupal.ajax.instances === 'undefined') {
+        for(var i in Drupal.ajax) { if (isAjaxing(Drupal.ajax[i])) { d7_not_ajaxing = false; } }
+      }
+      var d8_not_ajaxing = (typeof Drupal === 'undefined' || typeof Drupal.ajax === 'undefined' || typeof Drupal.ajax.instances === 'undefined' || !Drupal.ajax.instances.some(isAjaxing))
       return (
         // Assert no AJAX request is running (via jQuery or Drupal) and no
         // animation is running.
         (typeof jQuery === 'undefined' || (jQuery.active === 0 && jQuery(':animated').length === 0)) &&
-        (typeof Drupal === 'undefined' || typeof Drupal.ajax === 'undefined' || !Drupal.ajax.instances.some(isAjaxing))
+        d7_not_ajaxing && d8_not_ajaxing
       );
     }());
 JS;
