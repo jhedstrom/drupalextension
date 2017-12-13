606dd7f4ce310d4654f1af43dfb279378aa6c16e
Matthew Oliveira <m4olivei@gmail.com>
2017-12-13 15:59:26 -0500

    #446 Check for session first before logging out in fastLogout method of DrupalAuthenticationManager

--- src/Drupal/DrupalExtension/Manager/DrupalAuthenticationManager.php
+++ src/Drupal/DrupalExtension/Manager/DrupalAuthenticationManager.php
@@ -130,7 +130,10 @@ class DrupalAuthenticationManager implements DrupalAuthenticationManagerInterfac
      */
     public function fastLogout()
     {
-        $this->getSession()->reset();
-        $this->userManager->setCurrentUser(false);
+        $session = $this->getSession();
+        if ($session->isStarted()) {
+          $session->reset();
+          $this->userManager->setCurrentUser(FALSE);
+        }
     }
 }
