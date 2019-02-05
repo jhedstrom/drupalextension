7c7c9a5f5fa1e22a061f2d9c7ed1d3f855af0067
Pieter Frenssen <pieter@frenssen.be>
2019-02-05 15:46:08 +0100

    Throw a helpful exception if the AJAX timeout is missing due to a common mistake in the configuration.

--- src/Drupal/DrupalExtension/Context/MinkContext.php
+++ src/Drupal/DrupalExtension/Context/MinkContext.php
@@ -152,7 +152,11 @@ class MinkContext extends MinkExtension implements TranslatableContext
       );
     }());
 JS;
-        $result = $this->getSession()->wait(1000 * $this->getMinkParameter('ajax_timeout'), $condition);
+        $ajax_timeout = $this->getMinkParameter('ajax_timeout');
+        if ($ajax_timeout === null) {
+            throw new \Exception('No AJAX timeout has been defined. Please verify that "Drupal\MinkExtension" is configured in behat.yml (and not "Behat\MinkExtension").');
+        }
+        $result = $this->getSession()->wait(1000 * $ajax_timeout, $condition);
         if (!$result) {
             if ($event) {
                 /** @var \Behat\Behat\Hook\Scope\BeforeStepScope $event */
